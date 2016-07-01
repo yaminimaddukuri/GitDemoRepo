c4de327e97b056293ae8e5489af9b18c7cc1dacf
yamini maddukuri <yamini.maddukuri@gmail.com>
2016-07-01 15:19:18 +0530

    new file added

---
+++ HelloWorldProj/src/com/sic/gitdemo/NewFile.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sic.gitdemo;
+
+/**
+ * @author ymadduku
+ *
+ */
+public class NewFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("new file added");
+
+	}
+
+}

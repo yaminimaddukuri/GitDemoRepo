5f9ebdce1b7d6aac47bbaa41f4df321fc826b3dd
yamini maddukuri <yamini.maddukuri@gmail.com>
2016-07-01 15:10:28 +0530

    initial check-in

---
+++ HelloWorldProj/src/com/sic/gitdemo/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sic.gitdemo;
+
+/**
+ * @author ymadduku
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+	}
+
+}
